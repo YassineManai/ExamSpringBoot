ecbb2937d18db44fed94e4a759579b82df4b7e25
marwaniiwael18 <marwaniwael88@gmail.com>
2025-01-08 17:47:39 +0100

    Initial commit

---
+++ src/main/java/tn/esprit/examn2025/Examn2025Application.java
@@ -0,0 +1,13 @@
+package tn.esprit.examn2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examn2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Examn2025Application.class, args);
+    }
+
+}
---
+++ src/test/java/tn/esprit/examn2025/Examn2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.examn2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examn2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
